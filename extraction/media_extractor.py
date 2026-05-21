d377ae3f7881667688e67e3ba3f956224d2c184b
MRishitha015 <rishithamutteni@gmail.com>
2026-05-22 00:13:36 +0530

    create media extraction module skeleton

---
+++ extraction/media_extractor.py
@@ -0,0 +1,9 @@
+class MediaExtractor:
+    def __init__(self, video_path):
+        self.video_path = video_path
+
+    def extract_frames(self):
+        print("Frame extraction started")
+
+    def extract_audio(self):
+        print("Audio extraction started")
