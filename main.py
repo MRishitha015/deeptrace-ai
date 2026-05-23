448e37a47d579241fe15bbabddb1b9838427339a
MRishitha015 <rishithamutteni@gmail.com>
2026-05-23 17:52:55 +0530

    add basic API health and version endpoints

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "DeepTrace AI API Running"}
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
+
+
+@app.get("/version")
+def version():
+    return {"version": "v0.1"}
